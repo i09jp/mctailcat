fb94358773f74119252fbc5d429959729e326b01
Kisaraki2021 <yutoi0116@outlook.jp>
2026-09-12 23:17:03 +0900

    バージョン情報の定数名を修正し、Tags.Versionに更新

--- src/main/java/jp/i09/mctailcat/MCtailcat.java
+++ src/main/java/jp/i09/mctailcat/MCtailcat.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = MCtailcat.MODID, version = TAG.Version, name = "MCtailcat", acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = MCtailcat.MODID, version = Tags.Version, name = "MCtailcat", acceptedMinecraftVersions = "[1.7.10]")
 public class MCtailcat {
 
     public static final String MODID = "mctailcat";
